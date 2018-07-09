aad177b15432ee85b7977f031e0f15db3fa82762
Ricardo Delgado <ricdelgado80@gmail.com>
2018-07-09 17:42:57 -0400

    v.0.1 for fx-carousel

--- carousel.js
+++ carousel.js
@@ -15,47 +15,81 @@ customElements.define('fx-carousel', class extends HTMLElement {
   constructor () {
     super()
 
-    var fxCarousel = document.createElement('template')
-    var prevArrow = document.createElement('div')
-    prevArrow.setAttribute('class', 'fx-carousel-previous')
-    var leftArrow = document.createElement('span')
-    var centerSection = document.createElement('slot')
-    centerSection.setAttribute('name', 'current-image')
-    var nextArrow = document.createElement('div')
-    prevArrow.setAttribute('class', 'fx-carousel-next')
-    var rightArrow = document.createElement('span')
+    const shadowRoot = this.attachShadow({mode: 'open'})
 
-    // set arrow svg
-    leftArrow.innerHTML = '<svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z" fill="#fff"/></svg>'
-    rightArrow.innerHTML = '<svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" fill="#fff"/></svg>'
+    const fxCarousel = document.createElement('div')
+    fxCarousel.setAttribute('class', 'fx-carousel-holder')
+    fxCarousel.innerHTML = `
+      <div class="prevSlide">
+        <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z" fill="#fff"/></svg>
+      </div>
+      <slot></slot>
+      <div class="nextSlide">
+        <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" fill="#fff"/></svg>      
+      </div>
+    `
+
+    let sliderImages = this.querySelectorAll('fx-carousel img')
 
     // shadow root style
     const style = document.createElement('style')
     style.textContent = `
-      template {
-        display: inherit;
-        height: 500px;
+      .fx-carousel-holder {
+        display: inline-flex;
+      }
+
+      .fx-carousel-holder img {
+        width: 100%;
+      }
+
+      .prevSlide, .nextSlide {
+        position: relative;
+      }
+
+      .prevSlide svg, .nextSlide svg {
+        position: absolute;
+        top: 50%;
+        bottom: 50%;
+        transform: translate(-50%, -50%)
       }
     `
 
-    // set first image
-    let sliderImages = this.querySelectorAll('fx-carousel img')
-    centerSection.innerHTML = sliderImages[0].outerHTML
-
-    // shadow root
-    const shadowRoot = this.attachShadow({mode: 'open'})
-    shadowRoot.appendChild(style)
     shadowRoot.appendChild(fxCarousel)
-    fxCarousel.appendChild(prevArrow)
-    prevArrow.appendChild(leftArrow)
-    fxCarousel.appendChild(centerSection)
-    fxCarousel.appendChild(nextArrow)
-    nextArrow.appendChild(rightArrow)
+    shadowRoot.appendChild(style)
 
-    console.log(this)
-  }
-  
-  connectedCallback () {
+    let currentSlide = 0
 
+    function hideSlides(selectedSlide) {
+      sliderImages.forEach(img => {
+        if (img !== sliderImages[selectedSlide]) img.style.display = 'none'
+        else img.style.display = 'flex'
+      })
+
+      nextSlide.style.display = 'inherit'
+      previousSlide.style.display = 'inherit'
+      console.log(currentSlide, sliderImages.length)
+      if (currentSlide >= sliderImages.length - 1) nextSlide.style.display = 'none'
+      if (currentSlide <= 0) previousSlide.style.display = 'none'
+    }
+
+    function changeSlides(eventName) {
+      if (eventName === 'nextSlide') {
+        currentSlide++
+        hideSlides(currentSlide)
+      } else if (eventName === 'prevSlide') {
+        currentSlide--
+        hideSlides(currentSlide)
+      } else {
+        console.warn('Error with carousel selection')
+      }
+    }
+
+    let previousSlide = this.shadowRoot.querySelector('.prevSlide')
+    previousSlide.addEventListener('click', () => changeSlides('prevSlide'))
+
+    let nextSlide = this.shadowRoot.querySelector('.nextSlide')
+    nextSlide.addEventListener('click', () => changeSlides('nextSlide'))
+
+    hideSlides(currentSlide)
   }
 })
