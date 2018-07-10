f24004618b0140ef04384b31bcbb30d7e37e26de
Ricardo Delgado <ricdelgado80@gmail.com>
2018-07-09 21:32:26 -0400

    fx-carousel building on custom element

--- carousel.js
+++ carousel.js
@@ -1,95 +1,113 @@
 /*
-<fx-carousel>
-attributes:
-1. height
-2. img paths
+cases
 
-* shadow dom
-* slot for all the images
-* next and prev buttons
-* div that holds all of 
+half page for prev - next
+
+flex-container for images, not display-none
+
+js animations for slide - easing functions
+
+play and pause functions
+
+look into other constructors, etc
+
 
 */
 
-customElements.define('fx-carousel', class extends HTMLElement {
+class FxCarousel extends HTMLElement {
   constructor () {
     super()
-
     const shadowRoot = this.attachShadow({mode: 'open'})
+    this.currentSlide = 0
+    
+  }
 
-    const fxCarousel = document.createElement('div')
-    fxCarousel.setAttribute('class', 'fx-carousel-holder')
-    fxCarousel.innerHTML = `
-      <div class="prevSlide">
-        <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z" fill="#fff"/></svg>
-      </div>
-      <slot></slot>
-      <div class="nextSlide">
-        <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" fill="#fff"/></svg>      
+  connectedCallback() {
+
+    this.shadowRoot.innerHTML = `
+      <div class="fx-carousel-holder">
+        <div class="prev-slide">
+          <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z" fill="#fff"/></svg>
+        </div>
+        <slot></slot>
+        <div class="next-slide">
+          <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" fill="#fff"/></svg>      
+        </div>
       </div>
     `
-
-    let sliderImages = this.querySelectorAll('fx-carousel img')
 
     // shadow root style
     const style = document.createElement('style')
+
     style.textContent = `
       .fx-carousel-holder {
-        display: inline-flex;
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        height: 600px;
       }
 
-      .fx-carousel-holder img {
-        width: 100%;
+      img {
+        width: 500px;
       }
 
-      .prevSlide, .nextSlide {
-        position: relative;
+      .prev-slide, .next-slide {
+
       }
 
-      .prevSlide svg, .nextSlide svg {
-        position: absolute;
-        top: 50%;
-        bottom: 50%;
-        transform: translate(-50%, -50%)
+      .prev-slide svg, .next-slide svg {
+        
       }
     `
 
-    shadowRoot.appendChild(fxCarousel)
-    shadowRoot.appendChild(style)
+    this.shadowRoot.appendChild(style)
 
-    let currentSlide = 0
+    this.previousSlide = this.shadowRoot.querySelector('.prev-slide')
+    this.nextSlide = this.shadowRoot.querySelector('.next-slide')
+    this.sliderImages = Array.from(document.querySelectorAll('fx-carousel img'))
 
-    function hideSlides(selectedSlide) {
-      sliderImages.forEach(img => {
-        if (img !== sliderImages[selectedSlide]) img.style.display = 'none'
-        else img.style.display = 'flex'
-      })
+    this.previousSlide.addEventListener('click', () => this.changeSlides('prevSlide'))
+    this.nextSlide.addEventListener('click', () => this.changeSlides('nextSlide'))
 
-      nextSlide.style.display = 'inherit'
-      previousSlide.style.display = 'inherit'
-      console.log(currentSlide, sliderImages.length)
-      if (currentSlide >= sliderImages.length - 1) nextSlide.style.display = 'none'
-      if (currentSlide <= 0) previousSlide.style.display = 'none'
-    }
-
-    function changeSlides(eventName) {
-      if (eventName === 'nextSlide') {
-        currentSlide++
-        hideSlides(currentSlide)
-      } else if (eventName === 'prevSlide') {
-        currentSlide--
-        hideSlides(currentSlide)
-      } else {
-        console.warn('Error with carousel selection')
-      }
-    }
-
-    let previousSlide = this.shadowRoot.querySelector('.prevSlide')
-    previousSlide.addEventListener('click', () => changeSlides('prevSlide'))
-
-    let nextSlide = this.shadowRoot.querySelector('.nextSlide')
-    nextSlide.addEventListener('click', () => changeSlides('nextSlide'))
-
-    hideSlides(currentSlide)
+    this.hideSlides(this.currentSlide)
   }
-})
+
+  // get nextSlide() {
+  //   this.changeSlides('nextSlide')
+  // }
+
+  // get previousSlide() {
+  //   this.changeSlides('prevSlide')
+  // }
+
+  hideSlides(selectedSlide) {
+
+    this.sliderImages.forEach(img => {
+      if (img !== this.sliderImages[selectedSlide]) img.style.display = 'none'
+      else img.style.display = 'flex'
+    })
+
+    this.nextSlide.style.display = 'inherit'
+    this.previousSlide.style.display = 'inherit'
+
+    if (this.currentSlide >= sliderImages.length - 1) nextSlide.style.display = 'none'
+    if (this.currentSlide <= 0) previousSlide.style.display = 'none'
+  }
+
+  changeSlides(eventName) {
+    if (eventName === 'nextSlide') {
+      this.currentSlide++
+      this.hideSlides(this.currentSlide)
+    } else if (eventName === 'prevSlide') {
+      this.currentSlide--
+      this.hideSlides(this.currentSlide)
+    } else {
+      console.warn('Error with carousel selection')
+    }
+  }
+
+}
+
+customElements.define('fx-carousel', FxCarousel)
