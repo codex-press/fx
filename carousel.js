e55c54da601def0407d9ef66c05de0171594ea84
Ricardo Delgado <ricdelgado80@gmail.com>
2018-07-11 15:28:39 -0400

    carousel updates

--- carousel.js
+++ carousel.js
@@ -1,8 +1,6 @@
 /*
 cases
 
-half page for prev - next
-
 flex-container for images, not display-none
 
 js animations for slide - easing functions
@@ -18,47 +16,87 @@ class FxCarousel extends HTMLElement {
   constructor () {
     super()
     const shadowRoot = this.attachShadow({mode: 'open'})
-    this.currentSlide = 0
-    
+    this.totalSlideWidth = 0
+    this.currentSlideLocation = 0
+
   }
 
   connectedCallback() {
 
     this.shadowRoot.innerHTML = `
-      <div class="fx-carousel-holder">
-        <div class="prev-slide">
-          <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z" fill="#fff"/></svg>
-        </div>
+      <div class="image-strip">
         <slot></slot>
-        <div class="next-slide">
-          <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" fill="#fff"/></svg>      
-        </div>
+      </div>
+      <div class="prev-slide">
+        <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z" fill="#fff"/></svg>
+      </div>
+      <div class="next-slide">
+        <svg width="50" height="50" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z" fill="#fff"/></svg>      
       </div>
     `
 
     // shadow root style
     const style = document.createElement('style')
 
+    this.sliderImages = Array.from(document.querySelectorAll('fx-carousel img'))
+
+    this.sliderImages.forEach(img => {
+
+      setTimeout(() => {
+        this.totalSlideWidth += img.clientWidth
+
+        style.textContent += `
+          .image-strip {
+            display: flex;
+            transform: translate(${((this.totalSlideWidth - this.sliderImages[0].clientWidth) / 2)}px);
+            transition: transform .5s ease-out;
+          }
+        `
+      }, 10)
+    })
+
     style.textContent = `
-      .fx-carousel-holder {
+      :host {
         display: flex;
+        position: relative;
         flex-flow: row nowrap;
         justify-content: center;
         align-items: center;
         align-content: center;
-        height: 600px;
+        padding: 1em;
+        margin: 0 auto;
+        overflow: hidden;
       }
 
-      img {
-        width: 500px;
+      ::slotted(img) {
+        display: flex;
+        box-shadow: 5px 5px 7px 0 rgba(94,47,59,0.2);
+        z-index: 1;
       }
 
       .prev-slide, .next-slide {
-
+        display: flex;
+        position: absolute;
+        z-index: 10;
       }
 
-      .prev-slide svg, .next-slide svg {
-        
+      .prev-slide {
+        left: 0%;
+        top: 0%;
+        padding-left: -3em;
+        width: 49%;
+        height: 100%;
+        align-items: center;
+      }
+
+      .next-slide {
+        top: 0%;
+        left: 50%;
+        padding-right: -3em;
+        width: 49%;
+        height: 100%;
+        align-items: center;
+        justify-content: flex-end;
       }
     `
 
@@ -66,46 +104,59 @@ class FxCarousel extends HTMLElement {
 
     this.previousSlide = this.shadowRoot.querySelector('.prev-slide')
     this.nextSlide = this.shadowRoot.querySelector('.next-slide')
-    this.sliderImages = Array.from(document.querySelectorAll('fx-carousel img'))
+    this.slotImages = this.shadowRoot.querySelector('.image-strip')
+    this.fxCarousel = document.querySelector('fx-carousel')
 
-    this.previousSlide.addEventListener('click', () => this.changeSlides('prevSlide'))
-    this.nextSlide.addEventListener('click', () => this.changeSlides('nextSlide'))
+    this.previousSlide.addEventListener('click', () => this.previousSlideShift())
+    this.nextSlide.addEventListener('click', () => this.nextSlideShift())
 
-    this.hideSlides(this.currentSlide)
+    this.hideSlides(this.currentSlideLocation)
   }
-
-  // get nextSlide() {
-  //   this.changeSlides('nextSlide')
-  // }
-
-  // get previousSlide() {
-  //   this.changeSlides('prevSlide')
-  // }
 
   hideSlides(selectedSlide) {
+    setTimeout(() => {
+      let slideWidth = this.sliderImages[this.currentSlideLocation].clientWidth
+      this.fxCarousel.style.maxWidth = `${slideWidth}px`
+    }, 10)
 
-    this.sliderImages.forEach(img => {
-      if (img !== this.sliderImages[selectedSlide]) img.style.display = 'none'
-      else img.style.display = 'flex'
-    })
+    if (this.currentSlideLocation >= this.sliderImages.length - 1) this.nextSlide.style.visibility = 'hidden'
+    else this.nextSlide.style.visibility = 'visible'
 
-    this.nextSlide.style.display = 'inherit'
-    this.previousSlide.style.display = 'inherit'
-
-    if (this.currentSlide >= sliderImages.length - 1) nextSlide.style.display = 'none'
-    if (this.currentSlide <= 0) previousSlide.style.display = 'none'
+    if (this.currentSlideLocation < 1) this.previousSlide.style.visibility = 'hidden'
+    else this.previousSlide.style.visibility = 'visible'
   }
 
-  changeSlides(eventName) {
-    if (eventName === 'nextSlide') {
-      this.currentSlide++
-      this.hideSlides(this.currentSlide)
-    } else if (eventName === 'prevSlide') {
-      this.currentSlide--
-      this.hideSlides(this.currentSlide)
-    } else {
-      console.warn('Error with carousel selection')
-    }
+  nextSlideShift() {
+    this.currentSlideLocation++
+    let nextSlideWidth = this.sliderImages[this.currentSlideLocation].clientWidth
+
+    this.fxCarousel.style.maxWidth = `${nextSlideWidth}px`
+
+    let values = window.getComputedStyle(this.slotImages).getPropertyValue('transform')
+        values = values.split(')')[0]
+        values = values.split(', ')
+
+    var translateValue = parseInt(values[4])
+
+    this.slotImages.style.transform = `translate(${translateValue - nextSlideWidth}px)`
+
+    this.hideSlides(this.currentSlideLocation)
+  }
+
+  previousSlideShift() {
+    this.currentSlideLocation--
+    let nextSlideWidth = this.sliderImages[this.currentSlideLocation].clientWidth
+    console.log('nSW', nextSlideWidth)
+
+    let values = window.getComputedStyle(this.slotImages).getPropertyValue('transform')
+        values = values.split(')')[0]
+        values = values.split(', ')
+
+    var translateValue = parseInt(values[4])
+
+    this.slotImages.style.transform = `translate(${translateValue + nextSlideWidth}px)`
+
+    this.hideSlides(this.currentSlideLocation) 
   }
 
 }
