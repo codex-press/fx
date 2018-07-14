69c6ad59a72c64390b616bd1965e4f3a56b6965a
Tim Farnam <twfarnam@gmail.com>
2018-07-14 09:44:30 -0500

    fixing first test

--- carousel.test.js
+++ carousel.test.js
@@ -1,26 +1,14 @@
-import * as carousel from './carousel.js'
+import './carousel.js'
 
 describe('Carousel', () => {
-  let fxCarousel
 
   beforeEach(() => {
-    fxCarousel = '<fx-carousel><img src=""></fx-carousel>'
-
     document.body.innerHTML = '<fx-carousel><img src=""></fx-carousel>'
   }) 
 
-  it('createElement', () => {
-    expect(document.body.innerHTML).to.equal(fxCarousel)
-  })
 
-  it('checkShadow', done => {
-    console.log(document.body)
-
-    let shadow = document.querySelector('#shadow-root')
-
-    expect(shadow).to.exist
-
-    done()
+  it('has a shadowDOM', () => {
+    assert(document.querySelector('fx-carousel').shadowRoot)
   })
 
 })
