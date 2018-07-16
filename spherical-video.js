a6c08d73deca07e18453c11680a8bc408732f19f
Tim Farnam <twfarnam@gmail.com>
2018-07-16 14:36:00 -0500

    renames v360 to spherical video, fixes problems

---
+++ spherical-video.js
@@ -0,0 +1,70 @@
+import { dom, env, article } from '/app/index.js';
+import Plugin from '/app/src/plugin.js'
+import * as Kaleidoscope from './lib/kaleidoscope.js';
+
+
+article.register('.spherical-video', class SphericalVideo extends Plugin {
+
+  constructor(args) {
+    super(args)
+
+    window.addEventListener('resize', () => this.resize())
+    window.addEventListener(
+      'touchmove',
+      event => event.preventDefault(),
+      { passive : false }
+    )
+
+    this.videoElem = this.el.children[0]
+    this.videoElem.style.display = 'none'
+
+    // XXX
+    // this.videoElem.muted = true
+
+    this.viewer = new Kaleidoscope.Video({
+      source: this.videoElem,
+      verticalPanning: false,
+      container: this.el,
+      width: this.el.clientWidth,
+      height: this.el.clientWidth * 0.5625,
+    })
+
+    this.bind({ onscreen: 'onscreen' })
+  }
+
+  resize() {
+    console.log('resize')
+    this.viewer.setSize({ 
+      width: this.el.clientWidth,
+      // XXX assumes 16:9 aspect ratio is desired
+      height: this.el.clientWidth * 0.5625,
+    })
+  }
+
+  offscreen() {
+    this.isOnscreen = false
+  }
+
+
+  onscreen() {
+    this.isOnscreen = true
+    if (this.videoElem.readyState === 2)
+      this.play()
+    else
+      this.videoElem.addEventListener('canplay', () => {
+        if (this.isOnscreen)
+          this.play()
+      })
+  }
+
+
+  play() {
+    console.log('play')
+    this.viewer.render()
+    this.viewer.play()
+  }
+
+
+})
+
+
